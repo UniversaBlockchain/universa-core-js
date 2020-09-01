d0dd7ba2f2a15ebf858821301e2f8966b585bbdb
anzhu <zhuravlev@asprime.com>
2020-09-01 15:08:27 +0300

    Add postinstall

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -2,50 +2,50 @@ const fs = require('fs');
 const path = require('path');
 const gentlyCopy = require('gently-copy');
 
-const moduleFolder = path.resolve(__dirname, '..', 'node_modules/unicrypto/dist');
+// const moduleFolder = path.resolve(__dirname, '..', 'node_modules/unicrypto/dist');
 
-let VERSION;
+// let VERSION;
 
-fs.readdirSync(moduleFolder).forEach(file => {
-  const parts = file.split('.');
-  if (parts[parts.length - 1] !== "wasm") return;
+// fs.readdirSync(moduleFolder).forEach(file => {
+//   const parts = file.split('.');
+//   if (parts[parts.length - 1] !== "wasm") return;
 
-  parts.shift();
-  parts.pop();
-  VERSION = parts.join('.');
-});
+//   parts.shift();
+//   parts.pop();
+//   VERSION = parts.join('.');
+// });
 
-// node_modules/unicrypto/bin
-let projectRoot = path.dirname(require.main.filename);
+// // node_modules/unicrypto/bin
+// let projectRoot = path.dirname(require.main.filename);
 
-projectRoot = path.dirname(projectRoot);
-projectRoot = path.dirname(projectRoot);
-projectRoot = path.dirname(projectRoot);
+// projectRoot = path.dirname(projectRoot);
+// projectRoot = path.dirname(projectRoot);
+// projectRoot = path.dirname(projectRoot);
 
 const distPaths = ['build', 'public', 'dist'];
 
-let copied = false;
+// let copied = false;
 let copiedBuild = false;
 
-distPaths.map(distPath => {
-  const relative = path.resolve(projectRoot, distPath);
+// distPaths.map(distPath => {
+//   const relative = path.resolve(projectRoot, distPath);
 
-  if (!fs.existsSync(relative)) return;
+//   if (!fs.existsSync(relative)) return;
 
-  if (distPath !== "public") return copyWASM(relative);
+//   if (distPath !== "public") return copyWASM(relative);
 
-  const jsPath = relative + '/js';
+//   const jsPath = relative + '/js';
 
-  if (fs.existsSync(jsPath)) copyWASM(jsPath);
-  else copyWASM(relative);
-});
+//   if (fs.existsSync(jsPath)) copyWASM(jsPath);
+//   else copyWASM(relative);
+// });
 
-if (!copied) console.log(`WARNING: Cannot find destination directory. Please, copy node_modules/unicrypto/dist/crypto.${VERSION}.wasm to your frontend public directory`);
+// if (!copied) console.log(`WARNING: Cannot find destination directory. Please, copy node_modules/unicrypto/dist/crypto.${VERSION}.wasm to your frontend public directory`);
 
-function copyWASM(destination) {
-  gentlyCopy([`../node_modules/unicrypto/dist/crypto.${VERSION}.wasm`], `${destination}/crypto.${VERSION}.wasm`);
-  copied = true;
-}
+// function copyWASM(destination) {
+//   gentlyCopy([`../node_modules/unicrypto/dist/crypto.${VERSION}.wasm`], `${destination}/crypto.${VERSION}.wasm`);
+//   copied = true;
+// }
 
 function copyBuild(destination) {
   gentlyCopy([`../dist/uni.v${CORE_VERSION}.min.js`], `${destination}/uni.v${CORE_VERSION}.min.js`);
